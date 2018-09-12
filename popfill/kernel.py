b08a6c43d1f30ac0e56b6e8deb0c0bf0a7373a8c
David Millman <dave@cs.unc.edu>
2018-09-12 14:23:24 -0700

    Remove trailing white space

--- popfill/kernel.py
+++ popfill/kernel.py
@@ -17,25 +17,25 @@ class Basic(object):
 def pop2msk(pop):
     mask = (pop == pop )
     return mask;
-    
+
 # this function converts a tiff file to a numpy array
 def tiff2np(pop_file):
     with rasterio.open(pop_file) as pop:
-    
+
         print('population surface:')
         print(pop.profile)
-    
+
         pop_crs = pop.crs
         pop_nodata = pop.nodata
         pop_profile = pop.profile.copy()
         pop_aff = pop.transform
-    
+
         pop_np = pop.read(1)
         pop_np[pop_np == pop_nodata] = np.NaN;
-        
+
         return pop_np;
 
-# converts the ocean / land tiff to a boolean mask     
+# converts the ocean / land tiff to a boolean mask
 def oceanmask(mask_file):
     mask_crs = mask.crs
     mask_nodata = mask.nodata
@@ -44,6 +44,6 @@ def oceanmask(mask_file):
     # note the origin is slightly different beetween pop and mask
     mask_np = mask.read(1)
     mask_np[mask_np == mask_nodata] = 0
-    
+
     mask_np = mask_np.astype(bool)
 
